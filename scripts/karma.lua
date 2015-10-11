3a0a8b37184f994d8f835a5eaaa295b5b6c1865f
Baris Metin <baris@metin.org>
2015-10-11 00:51:03 -0700

    implement karma module in lua

---
+++ scripts/karma.lua
@@ -0,0 +1,72 @@
+
+function strip(str)
+   return str:match("^%s*(.-)%s*$")
+end
+
+function ltrim(str, match)
+   return str:match("^"..match.."(.-)$")
+end
+
+function starts(str, match)
+   if str:len() < match:len() then
+      return false
+   elseif str:sub(0, match:len()) == match then
+      return true
+   end
+   return false
+end
+
+function ends(str, match)
+   if str:len() < match:len() then
+      return false
+   elseif str:sub(str:len()-match:len()+1, str:len()) == match then
+      return true
+   end
+   return false
+end
+
+function getKarma(word)
+   value = alf.brainGet("karma", word)
+   if value ~= "" then
+      return tonumber(value)
+   end
+   return nil
+end
+
+function updateWord(word, add)
+   word = word:sub(0, word:len()-2)
+   value = getKarma(word)
+   if value then
+      value = value + add
+   else
+      value = add
+   end
+   alf.brainPut("karma", word, value)
+end
+
+function updateWords(msg)
+   for word in msg:gmatch("%S+") do
+      if ends(word, "++") then
+         updateWord(word, 1)
+      elseif ends(word, "--") then
+         updateWord(word, -1)
+      end
+   end
+end
+
+function processMessage ()
+   msg = strip(alf.msg())
+   updateWords(msg)
+
+   if starts(msg, "karma") then
+      msg = strip(ltrim(msg, "karma "))
+      karma = {}
+      for word in msg:gmatch("%w+") do
+         value = getKarma(word)
+         if value ~= nil then
+            table.insert(karma, word .. " has a karma of " .. value)
+         end
+      end
+      return table.concat(karma, "\n")
+   end
+end
--- scripts/smart-mouth.lua
+++ scripts/smart-mouth.lua
@@ -2,10 +2,12 @@
 function processMessage ()
    msg = alf.msg()
    for word in msg:gmatch("%w+") do
-      value = alf.brainGet("whatis", word)
-      if value ~= "" then
-         msg = "Oh wait, wait! Did you say " .. word .. "?\n" .. word .. " is " .. value .. "."
-         return msg
+      if word ~= alf.name then
+         value = alf.brainGet("whatis", word)
+         if value ~= "" then
+            msg = "Oh wait, wait! Did you say " .. word .. "?\n" .. word .. " is " .. value .. "."
+            return msg
+         end
       end
    end
 end
